ba78090eb6a53746e9f9adeabf1c2cd4d3503b0e
KateZzh <ekaterin.pro2@gmail.com>
2023-05-15 22:41:35 +0300

    add progress bar .js

--- app.js
+++ app.js
@@ -21,7 +21,6 @@ const songs = [
     nameOfSong: "Monster made of memories",
     imgPath:
       "background-image: url(./assets/img/monster-made-of-memories.jpeg)",
-    audioTime: "03:06",
     like: false,
   },
   {
@@ -30,7 +29,6 @@ const songs = [
     artist: "Falling In Reverse",
     nameOfSong: "Voices in my head",
     imgPath: "background-image: url(./assets/img/voices_in_my_head.jpeg)",
-    audioTime: "03:11",
     like: false,
   },
   {
@@ -40,7 +38,6 @@ const songs = [
     nameOfSong: "Getting away with murder",
     imgPath:
       "background-image: url(./assets/img/getting_away_with_murder.jpeg)",
-    audioTime: "03:14",
     like: false,
   },
 ];
@@ -57,6 +54,7 @@ function song() {
   } else favBtn.style = "background-image: url(./assets/icons/favBtnAct.png)";
 }
 
+// Like
 favBtn.addEventListener("click", function () {
   if (songs[currentIndexSong].like == false) {
     songs[currentIndexSong].like = true;
@@ -67,8 +65,10 @@ favBtn.addEventListener("click", function () {
   }
 });
 
+// Default song
 song();
 
+// Play and pause
 playPauseBtn.addEventListener("click", function () {
   song();
 
@@ -83,10 +83,12 @@ playPauseBtn.addEventListener("click", function () {
   }
 });
 
+// Prev song
 prevBtn.addEventListener("click", function () {
-  if (currentIndexSong === 0) return;
-
   currentIndexSong--;
+
+  if (currentIndexSong < 0) currentIndexSong = songs.length - 1;
+
   song();
 
   audio.play();
@@ -94,13 +96,32 @@ prevBtn.addEventListener("click", function () {
   playPauseBtn.style = "background-image: url(./assets/icons/pauseBtn.png)";
 });
 
+// Next song
 nextBtn.addEventListener("click", function () {
-  if (currentIndexSong === songs.length - 1) return;
-
   currentIndexSong++;
+
+  if (currentIndexSong > songs.length - 1) currentIndexSong = 0;
+
   song();
 
   audio.play();
   flag = true;
   playPauseBtn.style = "background-image: url(./assets/icons/pauseBtn.png)";
+});
+
+// Progress bar
+audio.addEventListener("timeupdate", function () {
+  const progress = document.querySelector(".progress");
+  const timer = document.querySelector(".current-time");
+  const { currentTime, duration } = this;
+
+  progress.style = `width: ${(currentTime * 100) / duration}%`;
+
+  const sec = Math.trunc(currentTime % 60);
+  const min = Math.trunc(currentTime / 60);
+
+  const timeSec = sec < 10 ? `0${sec}` : `${sec}`;
+  const timeMin = min < 10 ? `0${min}` : `0${min}`;
+
+  timer.innerHTML = `${timeMin}:${timeSec}`;
 });
